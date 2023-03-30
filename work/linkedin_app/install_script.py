3163a08b7e007e56007f98d66c3a7cd5c809776b
albieta <alba.roma@estudiantat.upc.edu>
2023-03-30 19:06:43 +0200

    upgrade: install packages in a more elegant way

--- work/linkedin_app/install_script.py
+++ work/linkedin_app/install_script.py
@@ -1,5 +1,17 @@
-from subprocess import run, PIPE
+import subprocess, sys, os
 
-# Install the required packages from Git
-run(["pip3", "install", "git+https://github.com/tomquirk/linkedin-api.git"])
-run(["pip3", "install", "linkedin-api~=2.0.0a"])
+class Packages:
+    """
+    This Class installs required Packages or library
+    """
+
+    get_pckg = subprocess.check_output([sys.executable, '-m', 'pip', 'freeze'])
+    installed_packages = [r.decode().split('==')[0] for r in get_pckg.split()]
+    # List of your required packages
+    required_packages = ['git+https://github.com/tomquirk/linkedin-api.git', 'linkedin-api~=2.0.0a']
+    for packg in required_packages:
+        if packg in installed_packages:
+            pass
+        else:
+            print('installing package %s' % packg)
+            os.system('pip3 install ' + packg)
